1c8d1897adf67f586d5ad3c1a8ecd4e6f0fff5d2
Yehor <129083741+YehorBr@users.noreply.github.com>
2025-10-06 18:27:40 +0200

    save changes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+  <PersistGate  loading={null} persistor={persistor}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </PersistGate>
   </Provider>
 );
-// #2196F3
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,20 @@
 import { devToolsEnhancer } from "@redux-devtools/extension";
 import { legacy_createStore as createStore } from "redux";
 import { rootReducer } from "./reducer";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+import {
+  FLUSH, PAUSE,
+  PERSIST,   PURGE,
+  REGISTER, REHYDRATE
+} from 'redux-persist';
+
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const initialState = {
   contacts: [],
@@ -10,4 +24,6 @@ export const initialState = {
 
 const enhanser = devToolsEnhancer()
 
-export const store = createStore(rootReducer, enhanser)
+export const store = createStore(persistedReducer, enhanser)
+
+export const persistor = persistStore(store)
